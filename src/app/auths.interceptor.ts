213b6912af26965f6b3e627d168205d305e34117
Sajeeb404 <ashrafulislam070@gmail.com>
2025-04-09 22:38:48 +0600

    interceptor

--- src/app/auths.interceptor.ts
+++ src/app/auths.interceptor.ts
@@ -1,7 +1,30 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-export const authsInterceptor: HttpInterceptorFn = (req, next) => {
 
-  console.log('hello im intercepter')
+export const authsInterceptor: HttpInterceptorFn = (req, next) => {
+  
+console.log('hello im interceptor')
+  if (['POST', 'PUT', 'PATCH'].includes(req.method.toUpperCase())) {
+    const wrappedBody = {
+      header: {
+        requestId: crypto.randomUUID(), 
+        requestClient: 'greenlc',
+        requestType: req.url, 
+        requestSource: 'greenlc-portal',
+        requestVersion: '1.0',
+        requestTime: new Date().toISOString(),
+        requestTimeoutInSeconds: 20,
+        requestRetryCount: 0,
+        requestSourceService: 'create'
+      },
+      meta: {},
+      body: req.body
+    };
+
+    const modifiedReq = req.clone({ body: wrappedBody });
+    return next(modifiedReq);
+  }
+
+ 
   return next(req);
 };
